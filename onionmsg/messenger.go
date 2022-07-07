40f12957921a11c193124dea38df9d894efd1a3b
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-07-07 13:12:32 +0200

    onionmsg: add ability to send onion messages

---
+++ onionmsg/messenger.go
@@ -0,0 +1,183 @@
+package onionmsg
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/carlakc/boltnd/lnwire"
+	"github.com/lightninglabs/lndclient"
+	"github.com/lightningnetwork/lnd/routing/route"
+)
+
+const (
+	lookupPeerBackoffDefault  = time.Second * 1
+	lookupPeerAttemptsDefault = 5
+)
+
+var (
+	// ErrNoAddresses is returned when we can't find a node's address in the
+	// public graph.
+	ErrNoAddresses = errors.New("no advertised addresses")
+
+	// ErrNoConnection is returned if we don't successfully connect to our
+	// peer within our set number of retries.
+	ErrNoConnection = errors.New("peer not connected within wait period")
+)
+
+// Messenger houses the functionality to send and receive onion messages.
+type Messenger struct {
+	// lnd provides the lnd apis required for onion messaging.
+	lnd LndOnionMsg
+
+	// lookupPeerBackoff is the amount of time that we back off for when
+	// waiting to connect to a peer.
+	lookupPeerBackoff time.Duration
+
+	// lookupPeerAttempts is the number of times we try to lookup our peer
+	// once connected.
+	lookupPeerAttempts int
+}
+
+// NewOnionMessenger creates a new onion messenger.
+func NewOnionMessenger(lnd LndOnionMsg) *Messenger {
+	return &Messenger{
+		lnd:                lnd,
+		lookupPeerBackoff:  lookupPeerBackoffDefault,
+		lookupPeerAttempts: lookupPeerAttemptsDefault,
+	}
+}
+
+// SendMessage sends an onion message to the peer provided. If we are not
+// currently connected to a peer, the messenger will directly connect to it
+// and send the message.
+//
+// Note that this API doesn't currently include a payload for the message, it
+// just sends an empty onion message.
+func (m *Messenger) SendMessage(ctx context.Context, peer route.Vertex) error {
+	msg, err := customOnionMessage(peer)
+	if err != nil {
+		return fmt.Errorf("could not create message: %w", err)
+	}
+
+	// Check whether we're connected to the next peer, making an ad-hoc
+	// connection if we're not.
+	if err := m.lookupAndConnect(ctx, peer); err != nil {
+		return fmt.Errorf("lookup and connect: %w", err)
+	}
+
+	return m.lnd.SendCustomMessage(ctx, *msg)
+}
+
+// lookupAndConnect checks whether we have a connection with a peer, and  looks
+// it up in the graph and makes a connection if we're not already connected.
+func (m *Messenger) lookupAndConnect(ctx context.Context,
+	peer route.Vertex) error {
+
+	// If we're already peered with the node, exit early.
+	isPeer, err := m.findPeer(ctx, peer)
+	if err != nil {
+		return fmt.Errorf("find peer: %w", err)
+	}
+
+	if isPeer {
+		return nil
+	}
+
+	info, err := m.lnd.GetNodeInfo(ctx, peer, false)
+	if err != nil {
+		return fmt.Errorf("could not lookup node: %w", err)
+	}
+
+	if len(info.Addresses) == 0 {
+		return fmt.Errorf("%w: %v", ErrNoAddresses, peer)
+	}
+
+	// Make a permanent connection to the peer so that they don't get
+	// pruned because we don't have a channel with them.
+	err = m.lnd.Connect(ctx, peer, info.Addresses[0], true)
+	if err != nil {
+		return fmt.Errorf("could not connect to peer: %w", err)
+	}
+
+	// It takes some time for our peer to connect, so we
+	for i := 0; i < m.lookupPeerAttempts; i++ {
+		isPeer, err := m.findPeer(ctx, peer)
+		if err != nil {
+			return fmt.Errorf("find peer: %v", err)
+		}
+
+		if isPeer {
+			return nil
+		}
+
+		// If we're not yet peered with the node, back off (or exit
+		// if ctx is canceled).
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case <-time.After(m.lookupPeerBackoff):
+			continue
+		}
+	}
+
+	return ErrNoConnection
+}
+
+// findPeer looks for a peer's pubkey in our list of online peers.
+func (m *Messenger) findPeer(ctx context.Context, peer route.Vertex) (bool,
+	error) {
+
+	peers, err := m.lnd.ListPeers(ctx)
+	if err != nil {
+		return false, fmt.Errorf("list peers: %w", err)
+	}
+
+	// If we're already peers, we can just return early.
+	for _, currentPeer := range peers {
+		if currentPeer.Pubkey == peer {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// customOnionMessage creates an onion message to our peer and wraps it in
+// a custom lnd message.
+func customOnionMessage(peer route.Vertex) (*lndclient.CustomMessage, error) {
+	pubkey, err := btcec.ParsePubKey(peer[:])
+	if err != nil {
+		return nil, fmt.Errorf("invalid pubkey: %w", err)
+	}
+
+	path := []*btcec.PublicKey{
+		pubkey,
+	}
+
+	sessionKey, err := btcec.NewPrivateKey()
+	if err != nil {
+		return nil, fmt.Errorf("could not get session key: %w", err)
+	}
+
+	// Create and encode an onion message.
+	msg, err := createOnionMessage(path, sessionKey)
+	if err != nil {
+		return nil, fmt.Errorf("onion message creation failed: %v", err)
+	}
+
+	buf := new(bytes.Buffer)
+	if err := msg.Encode(buf, 0); err != nil {
+		return nil, fmt.Errorf("onion message encode: %w", err)
+	}
+
+	return &lndclient.CustomMessage{
+		Peer:    peer,
+		MsgType: lnwire.OnionMessageType,
+		Data:    buf.Bytes(),
+	}, nil
+}
---
+++ onionmsg/messenger_test.go
@@ -0,0 +1,260 @@
+package onionmsg
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/carlakc/boltnd/testutils"
+	"github.com/lightninglabs/lndclient"
+	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+type sendMessageTest struct {
+	name string
+
+	// peer is the peer to send our message to.
+	peer route.Vertex
+
+	// peerLookups is the number of times that we lookup our peer after
+	// connecting.
+	peerLookups int
+
+	// expectedErr is the error we expect.
+	expectedErr error
+
+	// setMock primes our lnd mock for the specific test case.
+	setMock func(*mock.Mock)
+}
+
+// TestSendMessage tests sending of onion messages using lnd's custom message
+// api.
+func TestSendMessage(t *testing.T) {
+	pubkeys := testutils.GetPubkeys(t, 1)
+
+	pubkey, err := route.NewVertexFromBytes(
+		pubkeys[0].SerializeCompressed(),
+	)
+	require.NoError(t, err, "pubkey")
+
+	var (
+		peerList = []lndclient.Peer{
+			{
+				Pubkey: pubkey,
+			},
+		}
+		nodeAddr = "host:port"
+
+		privateNodeInfo = &lndclient.NodeInfo{
+			Node: &lndclient.Node{},
+		}
+
+		nodeInfo = &lndclient.NodeInfo{
+			Node: &lndclient.Node{
+				Addresses: []string{
+					nodeAddr,
+				},
+			},
+		}
+
+		listPeersErr = errors.New("listpeers failed")
+		getNodeErr   = errors.New("get node failed")
+		connectErr   = errors.New("connect failed")
+	)
+
+	tests := []sendMessageTest{
+		{
+			name:        "success - peer already connected",
+			peer:        pubkey,
+			peerLookups: 5,
+			expectedErr: nil,
+			setMock: func(m *mock.Mock) {
+				// We are already connected to the peer.
+				testutils.MockListPeers(m, peerList, nil)
+
+				// Send the message to the peer.
+				testutils.MockSendAnyCustomMessage(m, nil)
+			},
+		},
+		{
+			name:        "failure - list peers fails",
+			peer:        pubkey,
+			peerLookups: 5,
+			expectedErr: listPeersErr,
+			setMock: func(m *mock.Mock) {
+				testutils.MockListPeers(m, nil, listPeersErr)
+			},
+		},
+		{
+			name:        "failure - peer not found in graph",
+			peer:        pubkey,
+			peerLookups: 5,
+			expectedErr: getNodeErr,
+			setMock: func(m *mock.Mock) {
+				// We have no peers at present.
+				testutils.MockListPeers(m, nil, nil)
+
+				// Fail because we can't find the peer in the
+				// graph.
+				testutils.MockGetNodeInfo(
+					m, pubkey, false, nil, getNodeErr,
+				)
+			},
+		},
+		{
+			name:        "failure - peer has no addresses",
+			peer:        pubkey,
+			peerLookups: 5,
+			expectedErr: ErrNoAddresses,
+			setMock: func(m *mock.Mock) {
+				// We have no peers at present.
+				testutils.MockListPeers(m, nil, nil)
+
+				// Peer lookup succeeds, but there are no
+				// addresses listed.
+				testutils.MockGetNodeInfo(
+					m, pubkey, false, privateNodeInfo, nil,
+				)
+			},
+		},
+
+		{
+			name:        "failure - could not connect to peer",
+			peer:        pubkey,
+			expectedErr: connectErr,
+			setMock: func(m *mock.Mock) {
+				// We have no peers at present.
+				testutils.MockListPeers(m, nil, nil)
+
+				// Find the peer in the graph.
+				testutils.MockGetNodeInfo(
+					m, pubkey, false, nodeInfo, nil,
+				)
+
+				// Try to connect to the address provided, fail.
+				testutils.MockConnect(
+					m, pubkey, nodeAddr, true, connectErr,
+				)
+			},
+		},
+		{
+			name:        "success - peer immediately found",
+			peer:        pubkey,
+			peerLookups: 5,
+			expectedErr: nil,
+			setMock: func(m *mock.Mock) {
+				// We have no peers at present.
+				testutils.MockListPeers(m, nil, nil)
+
+				// Find the peer in the graph.
+				testutils.MockGetNodeInfo(
+					m, pubkey, false, nodeInfo, nil,
+				)
+
+				// Succeed in connecting to the address
+				// provided.
+				testutils.MockConnect(
+					m, pubkey, nodeAddr, true, nil,
+				)
+
+				// After connecting, immediately return the
+				// target peer from listpeers.
+				testutils.MockListPeers(m, peerList, nil)
+
+				// Send the message to the peer.
+				testutils.MockSendAnyCustomMessage(m, nil)
+			},
+		},
+		{
+			name:        "success - peer found after retry",
+			peer:        pubkey,
+			peerLookups: 5,
+			expectedErr: nil,
+			setMock: func(m *mock.Mock) {
+				// We have no peers at present.
+				testutils.MockListPeers(m, nil, nil)
+
+				// Find the peer in the graph.
+				testutils.MockGetNodeInfo(
+					m, pubkey, false, nodeInfo, nil,
+				)
+
+				// Succeed in connecting to the address
+				// provided.
+				testutils.MockConnect(
+					m, pubkey, nodeAddr, true, nil,
+				)
+
+				// In our first peer lookups, don't return the
+				// peer (mocking the time connection / handshake
+				// takes.
+				testutils.MockListPeers(m, nil, nil)
+				testutils.MockListPeers(m, nil, nil)
+
+				// On our third attempt, we're connected to the
+				// peer.
+				testutils.MockListPeers(m, peerList, nil)
+
+				// Send the message to the peer.
+				testutils.MockSendAnyCustomMessage(m, nil)
+			},
+		},
+		{
+			name:        "failure - peer not found after retry",
+			peer:        pubkey,
+			peerLookups: 2,
+			expectedErr: ErrNoConnection,
+			setMock: func(m *mock.Mock) {
+				// We have no peers at present.
+				testutils.MockListPeers(m, nil, nil)
+
+				// Find the peer in the graph.
+				testutils.MockGetNodeInfo(
+					m, pubkey, false, nodeInfo, nil,
+				)
+
+				// Succeed in connecting to the address
+				// provided.
+				testutils.MockConnect(
+					m, pubkey, nodeAddr, true, nil,
+				)
+
+				// The peer does not show up in our peer list
+				// after 2 calls.
+				testutils.MockListPeers(m, nil, nil)
+				testutils.MockListPeers(m, nil, nil)
+			},
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			testSendMessage(t, testCase)
+		})
+	}
+}
+
+func testSendMessage(t *testing.T, testCase sendMessageTest) {
+	// Create a mock and prime it for the calls we expect in this test.
+	lnd := testutils.NewMockLnd()
+	defer lnd.Mock.AssertExpectations(t)
+
+	testCase.setMock(lnd.Mock)
+
+	messenger := NewOnionMessenger(lnd)
+
+	// Overwrite our peer lookup defaults so that we don't have sleeps in
+	// our tests.
+	messenger.lookupPeerAttempts = testCase.peerLookups
+	messenger.lookupPeerBackoff = 0
+
+	ctxb := context.Background()
+
+	err := messenger.SendMessage(ctxb, testCase.peer)
+
+	// All of our errors are wrapped, so we can just check err.Is the
+	// error we expect (also works for nil).
+	require.True(t, errors.Is(err, testCase.expectedErr))
+}
