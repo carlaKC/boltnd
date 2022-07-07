f1e2594e6abb8ad2e83605b258fe6d98fecce3d6
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-07-07 11:14:27 +0200

    testing: add a common testing package with useful helpers

---
+++ testutils/vars.go
@@ -0,0 +1,8 @@
+package testutils
+
+var (
+	// pubkeyStrs holds a set of random public keys for testing.
+	pubkeyStrs = []string{
+		"031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f",
+	}
+)
