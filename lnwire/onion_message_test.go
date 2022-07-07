e75740835969c95c93e6934ebae1f7d366c9b04d
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-07-07 13:02:54 +0200

    lnwire: add onion message encoding/decoding

---
+++ lnwire/onion_message_test.go
@@ -0,0 +1,25 @@
+package lnwire
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/carlakc/boltnd/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+// TestOnionMessageEncode tests encoding and decoding of onion messages.
+func TestOnionMessageEncode(t *testing.T) {
+	pubkeys := testutils.GetPubkeys(t, 1)
+	blob := []byte{1, 2, 3}
+
+	expected := NewOnionMessage(pubkeys[0], blob)
+
+	buf := new(bytes.Buffer)
+	err := expected.Encode(buf, 0)
+	require.NoError(t, err, "encode onion")
+
+	actual := &OnionMessage{}
+	err = actual.Decode(buf, 0)
+	require.NoError(t, err, "onion decode")
+}
