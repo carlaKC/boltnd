ad7f2837c0a18c0bdc46f92b808b15c9e38fdc61
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-08-01 10:45:03 +0200

    offers: add invoice/offer validation

--- offers/coordinator.go
+++ offers/coordinator.go
@@ -1,15 +1,37 @@
 package offers
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"sync"
 	"sync/atomic"
+
+	"github.com/carlakc/boltnd/lnwire"
 )
 
 var (
 	// ErrShuttingDown is returned when the coordinator exits.
 	ErrShuttingDown = errors.New("coordinator shutting down")
+
+	// ErrOfferIDIncorrect is returned if an invoice's ID doesn't match
+	// our offer merkle root.
+	ErrOfferIDIncorrect = errors.New("invoice offer ID does not match " +
+		"original offer")
+
+	// ErrNodeIDIncorrect is returned when an invoice's node ID doesn't
+	// match the original offer.
+	ErrNodeIDIncorrect = errors.New("invoice node ID does not match " +
+		"original offer")
+
+	// ErrDescriptionIncorrect is returned when an invoice's description
+	// doesn't match the original offer.
+	ErrDescriptionIncorrect = errors.New("invoice description does not " +
+		"match original offer")
+
+	// ErrAmountIncorrect is returned if an invoice's amount is less than
+	// the minimum set by an offer.
+	ErrAmountIncorrect = errors.New("invoice amount < offer minimum")
 )
 
 // Coordinator manages the exchange of offer-related messages and payment of
@@ -79,4 +101,30 @@ func (c *Coordinator) handleOffers() error {
 			return ErrShuttingDown
 		}
 	}
+}
+
+// validateExchange validates that the messages that form part of a single
+// offer exchange are valid.
+func validateExchange(o *lnwire.Offer, i *lnwire.Invoice) error {
+	if !bytes.Equal(o.MerkleRoot[:], i.OfferID[:]) {
+		return fmt.Errorf("%w: %x merkle root, %x offer id",
+			ErrOfferIDIncorrect, o.MerkleRoot, i.OfferID)
+	}
+
+	if o.NodeID != i.NodeID {
+		return fmt.Errorf("%w: offer: %v, invoice: %v",
+			ErrNodeIDIncorrect, o.NodeID, i.NodeID)
+	}
+
+	if o.Description != i.Description {
+		return fmt.Errorf("%w: offer: %v, invoice: %v",
+			ErrDescriptionIncorrect, o.Description, i.Description)
+	}
+
+	if i.Amount < o.MinimumAmount {
+		return fmt.Errorf("%w: %v < %v", ErrAmountIncorrect, i.Amount,
+			o.MinimumAmount)
+	}
+
+	return nil
 }
