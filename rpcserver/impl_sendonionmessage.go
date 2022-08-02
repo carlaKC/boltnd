2ea12c971d18069ba9503a2ba0cd7293634010be
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-08-02 15:33:26 +0200

    multi: add final payload to sendmessage api

--- rpcserver/impl_sendonionmessage.go
+++ rpcserver/impl_sendonionmessage.go
@@ -3,8 +3,10 @@ package rpcserver
 import (
 	"context"
 
+	"github.com/carlakc/boltnd/lnwire"
 	"github.com/carlakc/boltnd/offersrpc"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/lightningnetwork/lnd/tlv"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -20,12 +22,12 @@ func (s *Server) SendOnionMessage(ctx context.Context,
 		return nil, err
 	}
 
-	pubkey, err := parseSendOnionMessageRequest(req)
+	pubkey, finalHop, err := parseSendOnionMessageRequest(req)
 	if err != nil {
 		return nil, err
 	}
 
-	err = s.onionMsgr.SendMessage(ctx, pubkey, nil)
+	err = s.onionMsgr.SendMessage(ctx, pubkey, finalHop)
 	if err != nil {
 		return nil, status.Errorf(
 			codes.Internal, "send message failed: %v", err,
@@ -39,14 +41,39 @@ func (s *Server) SendOnionMessage(ctx context.Context,
 // by SendOnionMessageRequest. All errors returned *must* include a grpc status
 // code.
 func parseSendOnionMessageRequest(req *offersrpc.SendOnionMessageRequest) (
-	route.Vertex, error) {
+	route.Vertex, []*lnwire.FinalHopPayload, error) {
 
 	pubkey, err := route.NewVertexFromBytes(req.Pubkey)
 	if err != nil {
-		return route.Vertex{}, status.Errorf(
+		return route.Vertex{}, nil, status.Errorf(
 			codes.InvalidArgument, "peer pubkey: %v", err,
 		)
 	}
 
-	return pubkey, nil
+	// If we have no final payloads, just return nil (this makes it easier
+	// for testing than having an empty slice).
+	if len(req.FinalPayloads) == 0 {
+		return pubkey, nil, nil
+	}
+
+	finalHopPayloads := make(
+		[]*lnwire.FinalHopPayload, 0, len(req.FinalPayloads),
+	)
+
+	for tlvType, payload := range req.FinalPayloads {
+		finalPayload := &lnwire.FinalHopPayload{
+			TLVType: tlv.Type(tlvType),
+			Value:   payload,
+		}
+
+		if err := finalPayload.Validate(); err != nil {
+			return route.Vertex{}, nil, status.Errorf(
+				codes.InvalidArgument, err.Error(),
+			)
+		}
+
+		finalHopPayloads = append(finalHopPayloads, finalPayload)
+	}
+
+	return pubkey, finalHopPayloads, nil
 }
