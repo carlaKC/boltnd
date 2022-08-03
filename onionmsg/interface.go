21f1cc3cc404a3fdc5ba3ce661be69d280301b3e
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-08-03 09:28:32 +0200

    onionmsg: add (de)registerto onion messenger interface and mock

--- onionmsg/interface.go
+++ onionmsg/interface.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lightninglabs/lndclient"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/routing/route"
+	"github.com/lightningnetwork/lnd/tlv"
 )
 
 // LndOnionMsg is an interface describing the lnd dependencies that the onionmsg
@@ -60,4 +61,14 @@ type OnionMessenger interface {
 	// optional TLVs for the target peer can be included in final payloads.
 	SendMessage(ctx context.Context, peer route.Vertex,
 		finalPayloads []*lnwire.FinalHopPayload) error
+
+	// RegisterHandler adds a handler onion message payloads delivered to
+	// our node for the tlv type provided.
+	// Note: this function will fail if the messenger has not been started.
+	RegisterHandler(tlvType tlv.Type, handler OnionMessageHandler) error
+
+	// DeregisterHandler removes a handler for onion message payloads for
+	// the tlv type provided.
+	// Note: this function will fail if the messenger has not been started.
+	DeregisterHandler(tlvType tlv.Type) error
 }
