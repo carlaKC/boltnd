e0877bfb364fb15efaee872bcd9e69a62dbaf61f
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-09-06 14:12:53 +0200

    itest: extract common onion payload subscription utilities

--- itest/common.go
+++ itest/common.go
@@ -1,7 +1,10 @@
 package itest
 
 import (
+	"errors"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/carlakc/boltnd/offersrpc"
 	"github.com/stretchr/testify/require"
@@ -28,5 +31,59 @@ func assertBlindedPathEqual(t *testing.T, expected,
 
 		require.Equal(t, hop.EncryptedData,
 			actual.Hops[i].EncryptedData, "encrypted data", i)
+	}
+}
+
+// consumeOnionMessage sets up a closure that can be used to consume messages
+// delivered from an onion message subscription client.
+//
+// This function calls Recv() in a goroutine so that tests will not block in
+// the case where we expect to receive a message but one does not arrive. An
+// alternative here would be to use a context with timeout on the top level
+// subscription, but this would require callers to know how long the test will
+// take to run (or overestimate it).
+func consumeOnionMessage(wg *sync.WaitGroup,
+	msgChan chan *offersrpc.SubscribeOnionPayloadResponse,
+	errChan chan error) func(client offersrpc.Offers_SubscribeOnionPayloadClient) {
+
+	return func(client offersrpc.Offers_SubscribeOnionPayloadClient) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			msg, err := client.Recv()
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			msgChan <- msg
+		}()
+	}
+}
+
+// readOnionMessage sets up a closure that will read responses from our onion
+// message subscription channels or fail after a timeout.
+func readOnionMessage(msgChan chan *offersrpc.SubscribeOnionPayloadResponse,
+	errChan chan error) func() (*offersrpc.SubscribeOnionPayloadResponse,
+	error) {
+
+	return func() (*offersrpc.SubscribeOnionPayloadResponse, error) {
+		select {
+		// If we receive a message as expected, assert that it is of
+		// the correct type.
+		case msg := <-msgChan:
+			return msg, nil
+
+		// If we received an error, something went wrong.
+		case err := <-errChan:
+			return nil, err
+
+		// In the case of a timeout, let our test exit. This will
+		// cancel the receive goroutine (through context cancelation)
+		// and wait for it to exit.
+		case <-time.After(defaultTimeout):
+			return nil, errors.New("message read timeout")
+		}
 	}
 }
